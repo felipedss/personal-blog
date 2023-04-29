cfdee3bd5ff160093046527ec9325482c8553101
felipedss <feliperosadr@gmail.com>
2023-04-29 20:08:54 -0300

    first commit

---
+++ src/partials/CTA.tsx
@@ -0,0 +1,11 @@
+import {
+  Section,
+} from 'astro-boilerplate-components';
+
+const CTA = () => (
+  <Section>
+
+  </Section>
+);
+
+export { CTA };
---
+++ src/partials/Hero.tsx
@@ -0,0 +1,55 @@
+import {
+  GradientText,
+  HeroAvatar,
+  HeroSocial,
+  Section,
+} from 'astro-boilerplate-components';
+
+const Hero = () => (
+  <Section>
+    <HeroAvatar
+      title={
+        <>
+          Hi there, I'm <GradientText>Felipe</GradientText> 👋
+        </>
+      }
+      description={
+        <>
+          I am a Software Engineer and I have been creating web solutions since 2010. I currently work with different technologies, such as Java, Spring Boot, PostgreSQL, Keycloak, Docker, Microservices, Oracle, MongoDB. I am passionate about programming and learning technologies.
+        </>
+      }
+      avatar={
+        <img
+          className="h-80 w-64"
+          src="/assets/images/avatar.svg"
+          alt="Avatar image"
+          loading="lazy"
+        />
+      }
+      socialButtons={
+        <>
+          <a href="https://twitter.com/felipedsrosa">
+            <HeroSocial
+              src="/assets/images/twitter-icon.png"
+              alt="Twitter icon"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/felipe-da-rosa-42a72947/">
+            <HeroSocial
+              src="/assets/images/linkedin-icon.png"
+              alt="Linkedin icon"
+            />
+          </a>
+          {/* <a href="/">
+            <HeroSocial
+              src="/assets/images/youtube-icon.png"
+              alt="Youtube icon"
+            />
+          </a> */}
+        </>
+      }
+    />
+  </Section>
+);
+
+export { Hero };
